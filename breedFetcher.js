6f0d0d5bf991354f9704d06d1dc86c69edb599b0
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-25 00:53:29 +0000

    refactored to take out consolelogs and added index.js. now you call on index.js to search also moved some constants there

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,23 @@
 const request = require("request");
-const breed = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, response, body) => {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("Requested breed is not found. Please check spelling");
-    }
-    try {
-      console.log(data[0].description);
-    } catch (err) {
-      console.log("err", err.message);
-    }
-  }
-);
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log("Requested breed is not found. Please check spelling");
+      }
+      try {
+        console.log(data[0].description);
+      } catch (err) {
+        callback(err, null);
+      }
+    });
+};
+
+module.exports = { fetchBreedDescription };
